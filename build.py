3809503aa76fa90a81bde8aded7eb95beab7178f
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-12-24 12:04:55 +0200

    aedi: remove 'do not write bytecode'

--- build.py
+++ build.py
@@ -27,9 +27,6 @@ if sys.version_info < _min_version:
     print(f'This module requires Python {_min_version[0]}.{_min_version[1]}.{_min_version[2]} or newer')
     sys.exit(1)
 
-os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
-sys.dont_write_bytecode = True
-
 root_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(f'{root_path}{os.sep}core')
 
